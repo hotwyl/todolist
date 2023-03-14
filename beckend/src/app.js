e64b633ee51e6a48f0843039ffd437fbac8e62cf
William Camargo <44587916+hotwyl@users.noreply.github.com>
2023-03-14 20:46:29 -0300

    atualização

--- beckend/src/app.js
+++ beckend/src/app.js
@@ -7,10 +7,8 @@ app.use(express.json());
 app.set("json spaces", 3);
 
 app.use(cors({
-    //origin: ['https://www.3wonline.com', 'http://www.3wonline.com', 'https://www.3wonline.com.br', 'http://www.3wonline.com.br', 'https://3wonline.com', 'http://3wonline.com', 'https://3wonline.com.br', 'http://3wonline.com.br']
     origin: '*'
 }));
-
 
 app.use(router);
 
