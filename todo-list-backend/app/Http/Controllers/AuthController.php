419e45b668c3c3cadc99a1427f84cd8e065e2132
William Camargo <44587916+hotwyl@users.noreply.github.com>
2023-12-05 22:08:14 -0300

    auth

--- todo-list-backend/app/Http/Controllers/AuthController.php
+++ todo-list-backend/app/Http/Controllers/AuthController.php
@@ -158,11 +158,9 @@ class AuthController extends Controller
             $user->tokens()->delete();
 
             $return = [
-                'codigo' => Auth::user()->cod,
-                'login' => Auth::user()->login,
-                'nome' => Auth::user()->name,
-                'email' => Auth::user()->email,
-                'token' => Auth::user()->createToken('invoice')->plainTextToken
+                'nome' => $user->name,
+                'email' => $user->email,
+                'token' => $user->createToken('invoice')->plainTextToken
             ];
 
             return response()->json([
@@ -180,4 +178,227 @@ class AuthController extends Controller
 
     }
     
+    public function updateUser($request){
+        try {
+            // Validação dos dados
+            $validator = Validator::make($request, [
+                'name' => 'nullable|string|max:255',
+                'email' => 'nullable|string|email|max:255',
+                'password' => 'nullable|confirmed|min:8',
+                'status' => 'nullable|['.User::STATUS_INACTIVE.','.User::STATUS_ACTIVE.']',
+            ]);
+
+            if ($validator->fails()) {
+                return response()->json([
+                    'message' => 'Falha na validação dos dados.',
+                    'errors' => $validator->errors(),
+                ], 422);
+            }
+
+            // Obter o usuário autenticado
+            $user = Auth::user();
+
+            $dados = [];
+
+            if(isset($request['name'])){
+                $dados['name'] = $request['name'];
+            }
+
+            if(isset($request['email'])){
+                $dados['email'] = $request['email'];
+            }
+
+            if(isset($request['password'])){
+                $dados['password'] = bcrypt($request['password']);
+            }
+
+            if(isset($request['status'])){
+                $dados['status'] = $request['status'];
+            }   
+            
+            $usuario = User::where('id', $user->id)->update($dados);
+
+            return response()->json([
+                'status' => true,
+                'message' => 'Usuário Atualizado com sucesso.',
+                'usuario' => $usuario,
+
+            ], 200);
+
+        } catch (\Exception $ex) {
+            return response()->json(['status' => false, 'message' => $ex->getMessage()], 401);
+        }
+
+    }
+
+    public function forgotPassword($email){
+        try {
+
+            // Validação dos dados
+            $validator = Validator::make($request, [
+                'email' => 'required|email',
+            ]);
+
+            if ($validator->fails()) {
+                return response()->json([
+                    'message' => 'Falha na validação dos dados.',
+                    'errors' => $validator->errors(),
+                ], 422);
+            }
+
+            $user = User::where('email', $request['email'])->first();
+
+            if(!$user){
+                return response()->json([
+                    'status' => false,
+                    'message' => 'Usuário não encontrado.',
+                ], 404);
+            }
+
+            $token = $user->createToken('auth_token')->plainTextToken;
+
+            $user->notify(new ResetPasswordNotification($token));
+
+            // Enviar e-mail de redefinição de senha
+            //Mail::to($user->email)->send(new ResetPasswordMail($user, $token));
+
+            return response()->json([
+                'status' => true,
+                'message' => 'Email enviado com sucesso.',
+            ], 200);
+
+        } catch (\Exception $ex) {
+            return response()->json([
+                'status' => false,
+                'message' => $ex->getMessage()
+            ], 401);
+        }
+    }
+
+    public function resetPassword($request){
+        try {
+
+            // Validação dos dados
+            $validator = Validator::make($request, [
+                'email' => 'required|email',
+                'token' => 'required',
+                'password' => 'required|confirmed|min:8',
+            ]);
+
+            if ($validator->fails()) {
+                return response()->json([
+                    'message' => 'Falha na validação dos dados.',
+                    'errors' => $validator->errors(),
+                ], 422);
+            }
+
+            // Verificar se o e-mail existe na tabela de usuários
+            $user = User::where('email', $request['email'])->first();
+
+            if(!$user){
+                return response()->json([
+                    'status' => false,
+                    'message' => 'Usuário não encontrado.',
+                ], 404);
+            }
+
+            if(!$user->tokens()->where('token', hash('sha256', $request['token']))->first()){
+                return response()->json([
+                    'status' => false,
+                    'message' => 'Token inválido.',
+                ], 401);
+            }
+
+            // $user->password = bcrypt($request['password']);
+            // $user->save();
+
+            // Verificar se o token de redefinição de senha é válido
+            $status = Password::reset($request->only('email', 'password', 'password_confirmation', 'token'), function ($user, $password) {
+                $user->forceFill([
+                    'password' => Hash::make($password),
+                    'remember_token' => Str::random(60),
+                ])->save();
+            });
+
+            if ($status === Password::INVALID_TOKEN) {
+                return response()->json([
+                    'message' => 'Token de redefinição de senha inválido.',
+                ], 400);
+            }
+
+            return response()->json([
+                'status' => true,
+                'message' => 'Senha alterada com sucesso.',
+            ], 200);
+
+        } catch (\Exception $ex) {
+            return response()->json([
+                'status' => false,
+                'message' => $ex->getMessage()
+            ], 401);
+        }
+    }
+
+    public function updatePassword($request){
+        try {
+
+            // Validação dos dados
+            $validator = Validator::make($request, [
+                'current_password' => 'required',
+                'new_password' => 'required|confirmed|min:8',
+            ]);
+
+            if ($validator->fails()) {
+                return response()->json([
+                    'message' => 'Falha na validação dos dados.',
+                    'errors' => $validator->errors(),
+                ], 422);
+            }
+
+            // Obter o usuário autenticado
+            $user = Auth::user();
+
+            // Verificar se a senha atual fornecida está correta
+            if (!Hash::check($request->current_password, $user->password)) {
+                return response()->json([
+                    'message' => 'Senha atual incorreta.',
+                ], 400);
+            }
+
+            // Atualizar a senha do usuário
+            // $user->password = bcrypt($request['password']);
+            $user->password = Hash::make($request->new_password);
+            $user->save();
+
+            return response()->json([
+                'status' => true,
+                'message' => 'Senha alterada com sucesso.',
+            ], 200);
+
+        } catch (\Exception $ex) {
+            return response()->json([
+                'status' => false,
+                'message' => $ex->getMessage()
+            ], 401);
+        }
+    }
+
+    public function me(){
+        try {
+            $user = Auth::user();
+
+            return response()->json([
+                'status' => true,
+                'message' => 'Usuário Logado.',
+                'user' => $user
+            ], 200);
+
+        } catch (\Exception $ex) {
+            return response()->json([
+                'status' => false,
+                'message' => $ex->getMessage()
+            ], 401);
+        }
+
+    }
 }
--- todo-list-backend/routes/api.php
+++ todo-list-backend/routes/api.php
@@ -9,8 +9,13 @@ Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 Route::get('/unlogged', [AuthController::class, 'unlogged'])->name('unlogged');
 Route::get('/user', [AuthController::class, 'user'])->middleware('auth:sanctum');
+Route::post('/update-user', [AuthController::class, 'updateUser'])->middleware('auth:sanctum');
 Route::get('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
 Route::post('/refresh', [AuthController::class, 'refresh'])->middleware('auth:sanctum');
+Route::post('/forgot-password', [AuthController::class, 'forgotPassword']);
+Route::post('/reset-password', [AuthController::class, 'resetPassword']);
+Route::post('/update-password', [AuthController::class, 'updatePassword'])->middleware('auth:sanctum');
+Route::get('/me', [AuthController::class, 'me'])->name('me');
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::get('/tasks', [TaskController::class, 'index']);
